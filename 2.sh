e448a2045d0cb63763df4d828a94ce10cef3f00b
kakathic <kakathic@gmail.com>
2020-05-30 13:36:42 +0700

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -10,7 +10,9 @@ curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1ka.txt -o /
 tar -xf /storage/emulated/0/1ka.txt -C /storage/emulated/0/
 rm /storage/emulated/0/1ka.txt
 fi
-curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/clock_pro.mtz -o /system/media/theme/default/gadgets/clock_pro.mtz
+curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/clock_pro.mtz -o /storage/emulated/0/Download/clock_pro.mtz
+mv /storage/emulated/0/Download/clock_pro.mtz /system/media/theme/default/gadgets/clock_pro.mtz
+chmod 644 /system/media/theme/default/gadgets/clock_pro.mtz
 rm /storage/emulated/0/Download/2.sh
 rm /storage/emulated/0/Download/Clock_Pro_Root.sh
 curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/Clock_Pro_Root.sh -o /storage/emulated/0/Download/Clock_Pro_Root.sh
