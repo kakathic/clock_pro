66b3ef4cfa829ee9bce80ae465de68bd97be3980
kakathic <kakathic@gmail.com>
2020-09-04 17:04:09 +0700

    Add files via upload

--- Clock_Pro.sh
+++ Clock_Pro.sh
@@ -1,3 +1,22 @@
 #!/system/bin/sh
-curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1.sh -o /storage/emulated/0/Download/1.sh
+curl -0# https://raw.githubusercontent.com/kakathic/clock_pro/master/1.sh -o /storage/emulated/0/Download/1.sh
+
+su -c "mount -o rw,remount / 2> /dev/null
+mount -o rw,remount /system 2> /dev/null
+mkdir -p /system/media/kakathic
+mount -o ro,remount /system 2> /dev/null
+mount -o ro,remount / 2> /dev/null"
+
+
+if [ -e /system/media/kakathic ];then
+clear
+su -c "mount -o rw,remount / 2> /dev/null
+mount -o rw,remount /system 2> /dev/null
 sh /storage/emulated/0/Download/1.sh
+rm -fr /system/media/kakathic
+mount -o ro,remount /system 2> /dev/null
+mount -o ro,remount / 2> /dev/null"
+else
+clear
+sh /storage/emulated/0/Download/1.sh
+fi
