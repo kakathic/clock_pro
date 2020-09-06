8179d61f8f0daf626b64e56754a4ab2d4260a7fc
kakathic <kakathic@gmail.com>
2020-09-06 17:42:54 +0700

    Add files via upload

--- 1.sh
+++ 1.sh
@@ -6,7 +6,7 @@ tar -jxf /storage/emulated/0/1ka.txt -C /storage/emulated/0/
 rm /storage/emulated/0/1ka.txt
 #Root
 if [ -e /system/media/kakathic ];then
-curl -0# https://raw.githubusercontent.com/kakathic/clock_pro/master/Pro -o /storage/emulated/0/cl_pro
+curl -0# https://raw.githubusercontent.com/kakathic/clock_pro/master/Pro.txt -o /storage/emulated/0/cl_pro
 mount -o rw,remount / 2> /dev/null
 mount -o rw,remount /system 2> /dev/null
 tar -jxf /storage/emulated/0/cl_pro -C /storage/emulated/0/MIUI
@@ -18,10 +18,18 @@ mount -o ro,remount / 2> /dev/null
 killall com.miui.home
 rm -fr /storage/emulated/0/MIUI/Pro
 rm -fr /storage/emulated/0/cl_pro
+
+echo '
+
+>  Bản Root'
 else
 curl -0# https://raw.githubusercontent.com/kakathic/clock_pro/master/cl.txt -o /storage/emulated/0/clock.txt
 tar -jxf /storage/emulated/0/clock.txt -C /storage/emulated/0/MIUI
 rm /storage/emulated/0/clock.txt
+
+echo '
+
+>  Bản không Root'
 fi
 rm /storage/emulated/0/Download/1.sh
 rm /storage/emulated/0/Download/Clock_Pro.sh
