2c302f6a99d911600f071cf3acf796f61acb19f9
kakathic <kakathic@gmail.com>
2020-09-08 20:29:00 +0700

    Update Clock_Pro.sh

--- Clock_Pro.sh
+++ Clock_Pro.sh
@@ -1,14 +1,4 @@
 #!/system/bin/sh
 curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1.sh -o /storage/emulated/0/Download/1.sh
-mount -o rw,remount / 2> /dev/null
-mount -o rw,remount /system 2> /dev/null
-mkdir -p /system/media/kakathic
-mount -o ro,remount /system 2> /dev/null
-mount -o ro,remount / 2> /dev/null
-if [ -e /system/media/kakathic ];then
 clear
 sh /storage/emulated/0/Download/1.sh
-else
-clear
-sh /storage/emulated/0/Download/1.sh
-fi
