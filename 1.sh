a9173f6d1231c8a3c0dcc6840e2a5fa5a9e83a3c
kakathic <kakathic@gmail.com>
2021-02-09 13:37:04 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,10 +2,10 @@ if [ -e /sdcard/.1ka ];then
 rm -fr /sdcard/.1ka
 fi
 curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1ka.txt -o /sdcard/1ka.txt
-tar -jxf /sdcard/1ka.txt -C /sdcard/
+tar -xf /sdcard/1ka.txt -C /sdcard/
 rm /sdcard/1ka.txt
 curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/cl.txt -o /sdcard/clock.txt
-tar -jxf /sdcard/clock.txt -C /sdcard/MIUI
+tar -xf /sdcard/clock.txt -C /sdcard/MIUI
 rm /sdcard/clock.txt
 rm /sdcard/Download/Clock_Pro.sh
 curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/Clock_Pro.sh -o /sdcard/Download/Clock_Pro.sh
