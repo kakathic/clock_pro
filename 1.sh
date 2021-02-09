b452a6278ffa21006a86521a7d3a110cd607be61
kakathic <kakathic@gmail.com>
2021-02-09 13:25:35 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,14 +1,14 @@
-if [ -e /storage/emulated/0/.1ka ];then
-rm -fr /storage/emulated/0/.1ka
+if [ -e /sdcard/.1ka ];then
+rm -fr /sdcard/.1ka
 fi
-curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1ka.txt -o /storage/emulated/0/1ka.txt
-tar -jxf /storage/emulated/0/1ka.txt -C /storage/emulated/0/
-rm /storage/emulated/0/1ka.txt
-curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/cl.txt -o /storage/emulated/0/clock.txt
-tar -jxf /storage/emulated/0/clock.txt -C /storage/emulated/0/MIUI
-rm /storage/emulated/0/clock.txt
-rm /storage/emulated/0/Download/Clock_Pro.sh
-curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/Clock_Pro.sh -o /storage/emulated/0/Download/Clock_Pro.sh
+curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/1ka.txt -o /sdcard/1ka.txt
+tar -jxf /sdcard/1ka.txt -C /sdcard/
+rm /sdcard/1ka.txt
+curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/cl.txt -o /sdcard/clock.txt
+tar -jxf /sdcard/clock.txt -C /sdcard/MIUI
+rm /sdcard/clock.txt
+rm /sdcard/Download/Clock_Pro.sh
+curl -# https://raw.githubusercontent.com/kakathic/clock_pro/master/Clock_Pro.sh -o /sdcard/Download/Clock_Pro.sh
 
 echo '
 
